e334ef0a69aee6af4050496848702bd603597ded
David Williams-Young <dbwy@lbl.gov>
2020-09-23 18:27:51 +0000

    Add -fno-sycl-early-optimizations to build flags in SYCL impl to avoid issues in DPC++

--- src/sycl/exchcxx_sycl.cmake
+++ src/sycl/exchcxx_sycl.cmake
@@ -6,4 +6,13 @@ set( EXCHCXX_SYCL_SOURCES
 
 
 target_sources( exchcxx PRIVATE ${EXCHCXX_SYCL_SOURCES} )
+
+include(CheckCXXCompilerFlag)
+check_cxx_compiler_flag("-fno-sycl-early-optimizations" EXCHCXX_SYCL_HAS_NO_EARLY_OPTIMIZATIONS )
+if( EXCHCXX_SYCL_HAS_NO_EARLY_OPTIMIZATIONS )
+  target_compile_options( exchcxx PRIVATE
+    $<$<COMPILE_LANGUAGE:CXX>: -fno-sycl-early-optimizations>
+  )
+endif()
+
 #target_link_libraries( exchcxx PUBLIC SYCL::SYCL )
