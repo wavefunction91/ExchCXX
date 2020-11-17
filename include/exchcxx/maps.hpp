4ca650aeeaf505968c51a7125c902ea4ecbb9f9f
samslattery <samwise@vt.edu>
2020-11-17 15:35:36 -0500

    Made class (MapStr) to hold both forward and reverse maps for any type with std::string. Intended to be used for map with Kernels and Functionals.

--- include/exchcxx/maps.hpp
+++ include/exchcxx/maps.hpp
@@ -3,11 +3,29 @@
 
 namespace ExchCXX {
 
-std::map<std::string, XCFunctional::Functional> str2func = {
-    {"SVWN3", XCFunctional::Functional::SVWN3},
-    {"BLYP", XCFunctional::Functional::BLYP},
-    {"PBE0", XCFunctional::Functional::PBE0},
-    {"PBE_X", XCFunctional::Functional::PBE_X},
-    {"SlaterExchange", XCFunctional::Functional::SlaterExchange}};
+template <typename T> class MapStr : public std::map<std::string, T> {
+  std::map<std::string, T> str2val_;
+  std::map<T, std::string> val2str_;
+
+public:
+  MapStr(std::initializer_list<std::pair<const std::string, T>> strval)
+      : str2val_(strval) {
+
+    for (auto &&v : str2val_) {
+      val2str_.insert(std::make_pair(v.second, v.first));
+    }
+  }
+
+  T to_val(std::string str) { return str2val_.at(str); }
+
+  std::string to_str(T val) { return val2str_.at(val); }
+};
+
+MapStr<XCFunctional::Functional>
+    FuncMap({{"SVWN3", XCFunctional::Functional::SVWN3},
+             {"BLYP", XCFunctional::Functional::BLYP},
+             {"PBE0", XCFunctional::Functional::PBE0},
+             {"PBE_X", XCFunctional::Functional::PBE_X},
+             {"SlaterExchange", XCFunctional::Functional::SlaterExchange}});
 
 } // namespace ExchCXX
