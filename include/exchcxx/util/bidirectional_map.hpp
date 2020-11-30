df6f665c011575c6b904e664be1e3ba3de1022a2
samslattery <samwise@vt.edu>
2020-11-30 15:09:25 -0500

    Added template parameters (with defaults) to set the template parameters of compare functions of the forward and reverse maps

--- include/exchcxx/util/bidirectional_map.hpp
+++ include/exchcxx/util/bidirectional_map.hpp
@@ -3,13 +3,14 @@
 
 namespace ExchCXX {
 
-template <typename Key, typename Val>
+template <typename Key, typename Val, typename CompareKey = std::less<Key>,
+    typename CompareVal = std::less<Val> >
 class BidirectionalMap{
-  std::map<Key, Val> forward_map_;
-  std::map<Val, Key> reverse_map_;
+  std::map<Key, Val, CompareKey> forward_map_;
+  std::map<Val, Key, CompareVal> reverse_map_;
 
 public:
-  BidirectionalMap(std::map<Key, Val> map) : forward_map_(map){
+  BidirectionalMap(std::map<Key, Val, CompareKey> map) : forward_map_(map){
 
     for (auto &&v : map) {
       if (reverse_map_.find(v.second) != reverse_map_.end()){
