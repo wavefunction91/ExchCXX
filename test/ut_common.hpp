e565c55e23cd168fb02b242a0858a499bdfa6020
samslattery <samwise@vt.edu>
2021-01-05 13:51:37 -0500

    Added tests for kernel_map and functional_map

--- test/ut_common.hpp
+++ test/ut_common.hpp
@@ -66,4 +66,27 @@ static constexpr std::array builtin_supported_kernels = {
   ExchCXX::Kernel::PBE0
 };
 
+static constexpr std::array string_kernal_pairs = {
+    std::pair("SlaterExchange", ExchCXX::Kernel::SlaterExchange),
+    std::pair("PBE_X",ExchCXX::Kernel::PBE_X),
+    std::pair("PBE_C", ExchCXX::Kernel::PBE_C),
+    std::pair("LYP", ExchCXX::Kernel::LYP),
+    std::pair("B3LYP", ExchCXX::Kernel::B3LYP),
+    std::pair("PBE0", ExchCXX::Kernel::PBE0),
+    std::pair("VWN3", ExchCXX::Kernel::VWN3),
+    std::pair("VWN5", ExchCXX::Kernel::VWN5),
+    std::pair("PZ81", ExchCXX::Kernel::PZ81),
+    std::pair("PZ81_MOD", ExchCXX::Kernel::PZ81_MOD),
+    std::pair("PW91_LDA", ExchCXX::Kernel::PW91_LDA),
+    std::pair("PW91_LDA_MOD", ExchCXX::Kernel::PW91_LDA_MOD),
+    std::pair("PW91_LDA_RPA", ExchCXX::Kernel::PW91_LDA_RPA),
+    std::pair("B88", ExchCXX::Kernel::B88)
+};
 
+static constexpr std::array string_functional_pairs = {
+    std::pair("SVWN3", ExchCXX::Functional::SVWN3),
+    std::pair("SVWN5", ExchCXX::Functional::SVWN5),
+    std::pair("BLYP", ExchCXX::Functional::BLYP),
+    std::pair("B3LYP", ExchCXX::Functional::B3LYP),
+    std::pair("PBE0", ExchCXX::Functional::PBE0)
+};
