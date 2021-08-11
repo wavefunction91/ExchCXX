6bb305f1110e2893cfaadb62eb305ba617524af2
David Williams-Young <dbwy@lbl.gov>
2021-08-11 16:27:24 -0400

    [HIP] Revamp HIP CMake harnesss to use 3.21 goodies

--- src/hip/exchcxx_hip.cmake
+++ src/hip/exchcxx_hip.cmake
@@ -1,7 +1,10 @@
 set( EXCHCXX_HIP_SOURCES 
-  hip/xc_functional_device.cxx 
-  hip/libxc_device.cxx 
-  hip/builtin.cxx
+  hip/xc_functional_device.hip 
+  hip/libxc_device.hip 
+  hip/builtin.hip
 )
 
+find_package( hip REQUIRED )
+
 target_sources( exchcxx PRIVATE ${EXCHCXX_HIP_SOURCES} )
+target_link_libraries( exchcxx PUBLIC hip::host )
