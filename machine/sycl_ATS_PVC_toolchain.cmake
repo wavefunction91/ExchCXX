ef9700d38c2c58ff3458064efeec07d5603a14c1
Abhishek Bagusetty <abagusetty@anl.gov>
2022-07-20 21:40:49 +0000

    [SYCL] address PR comments

--- machine/sycl_ATS_PVC_toolchain.cmake
+++
@@ -1,6 +0,0 @@
-set( CMAKE_C_COMPILER   icx  )
-set( CMAKE_CXX_COMPILER icpx )
-
-set( CMAKE_CXX_FLAGS " -fsycl -fno-sycl-id-queries-fit-in-int -fsycl-device-code-split=per_kernel -fsycl-targets=spir64_gen -Xsycl-target-backend \"-device 12.1.0,12.4.0\" " )
-
-set( EXCHCXX_ENABLE_SYCL ON CACHE BOOL "" )
--- src/sycl/exchcxx_sycl.cmake
+++ src/sycl/exchcxx_sycl.cmake
@@ -7,20 +7,10 @@ set( EXCHCXX_SYCL_SOURCES
 
 target_sources( exchcxx PRIVATE ${EXCHCXX_SYCL_SOURCES} )
 
-#set(SYCL_FLAGS -fsycl -fsycl-unnamed-lambda -fsycl-device-code-split=per_kernel -fsycl-targets=spir64_gen -Xsycl-target-backend "-device 12.1.0,12.4.0")
-#set(SYCL_FLAGS -fsycl -fsycl-unnamed-lambda -fsycl-device-code-split=per_kernel -fsycl-targets=nvptx64-nvidia-cuda -Xsycl-target-backend --cuda-gpu-arch=sm_80)
-#set(SYCL_FLAGS -fsycl -fsycl-unnamed-lambda -fsycl-device-code-split=per_kernel -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx90a)
-
 include(CheckCXXCompilerFlag)
-target_compile_options( exchcxx
-  PRIVATE
-    $<$<COMPILE_LANGUAGE:CXX>: ${SYCL_FLAGS} > )
-
 check_cxx_compiler_flag("-fno-sycl-early-optimizations" EXCHCXX_SYCL_HAS_NO_EARLY_OPTIMIZATIONS )
 if( EXCHCXX_SYCL_HAS_NO_EARLY_OPTIMIZATIONS )
   target_compile_options( exchcxx PRIVATE
     $<$<COMPILE_LANGUAGE:CXX>: -fno-sycl-early-optimizations>
   )
 endif()
-
-target_link_libraries( exchcxx PUBLIC ${SYCL_FLAGS} )
